41a7d4008f5f41fb3713dd6a35fb9f785027b292
Viviane da Rosa Sommer <viviane.sommer@hitechnologies.com.br>
2023-02-20 21:27:02 -0300

    Tão falando as madame

--- Trabalho/cliente.c
+++ Trabalho/cliente.c
@@ -13,156 +13,69 @@
 #define PACKETS 10
 #define SIZE 1024
 
-/* Arquivo de logfile */
-FILE *logfile;
-
-/* Endereço para o qual responderei */
-struct sockaddr_in EnderecRemoto;
-
-/* Socket ID */
 int sd;
-
-/* Threads para enviarem e receberem dados simultaneamente */
-pthread_t S;
-pthread_t R;
-
-/* Estruturas utilizadas para o calculo do tempo de latencia */
-long tempos[PACKETS];
-long long tempo = 0;
-
-/* Contador de pacotes recebidos */
-int lidos = 0;
+struct hostent *h;
+struct sockaddr_in remoteServAddr;
 
 void enviarMensagem() {
 
-  struct timeval inicio;
-  char dados[SIZE];
-  for (int i=0; i<PACKETS; i++) {
-    sprintf(dados,"%*d",SIZE-1,i);
-    dados[SIZE-1]='\0';
-    gettimeofday(&inicio, NULL);
-    tempos[i] = inicio.tv_sec * 1000 + inicio.tv_usec/1000;
-    if (sendto(sd,dados,SIZE,0,(struct sockaddr *) &EnderecRemoto, sizeof(EnderecRemoto)) != SIZE) {
-        fprintf(stderr,"Nao foi possivel transmitir!\n");
-        printf("Nao foi possivel transmitir!\n");
-        exit(5);
-    }
-    fprintf(logfile,"Enviado pacote # %d.\n",i);
-    printf("Enviado pacote # %d.\n",i);
-  }
-
-
-  /* Apos enviar todos os pacotes, fica enviando uma mensagem final ('X')
-     para indicar o termino de envio de mensagens.
-     A thread que receberá os pacotes irá matar esta thread quando a
-     mensagem com X retornar para ela.                                    */
-  sprintf(dados,"%*cX",SIZE-2,' ');
-  dados[SIZE-1] = '\0';
-  while (1) {
-        sendto(sd,dados,SIZE,0,(struct sockaddr *) &EnderecRemoto,
-        sizeof(EnderecRemoto));
-  }
-  fprintf(logfile,"Fim do envio\n");
-}
-
-
-void receberMensagem(){
-    unsigned int tt;
-    int last=-1,wrong=0;
-    int x;
-    struct timeval end;
-
-    while (1) {
-        char buffer[SIZE];
-        recvfrom(sd,buffer,SIZE,0,(struct sockaddr *) &EnderecRemoto, &tt);
-        if (buffer[SIZE-2] == 'X') {
-            /* Se eu receber a mensagem final, mata a thread que envia e sai
-               do laco infinito.                                          */
-            pthread_cancel(S);
-            break;
-        } else {
-            // x conterá o ID do pacote recebido
-            sscanf(buffer,"%d",&x);
-            fprintf(logfile,"Recebido pacote # %d.\n",x);
-            lidos++;
-
-            // verifica se o pacote esta fora de ordem
-            if (x<=last) wrong++;
-            last=x;
-
-            // calcula a latencia para este pacote
-            gettimeofday(&end,NULL);
-            long mtime;
-            long tempo1 = tempos[x];
-            long tempo2 = end.tv_sec * 1000 + end.tv_usec/1000;
-
-            mtime = tempo2 - tempo1;
-            tempo += (long long)mtime;
+    char dados[SIZE];
+    for (int i=0; i<PACKETS; i++) {
+        sprintf(dados,"%*d",SIZE-1,i);
+        dados[SIZE-1]='\0';
+        if (sendto(sd,dados,SIZE,0,(struct sockaddr *) &remoteServAddr, sizeof(remoteServAddr)) != SIZE) {
+            printf("Nao foi possivel transmitir para o servidor!\n");
+            exit(1);
         }
+        printf("Enviado pacote # %d.\n",i);
     }
-    printf("Pacotes fora de ordem: %d (%.2lf %).\n",wrong,100.0*(double)wrong/(double)PACKETS);
-    fflush(stdout);
 
+    sprintf(dados,"%*cX",SIZE-2,' ');
+    dados[SIZE-1] = '\0';
+    if (sendto(sd,dados,SIZE,0,(struct sockaddr *) &remoteServAddr, sizeof(remoteServAddr)) != SIZE) {
+        printf("Nao foi possivel transmitir para o servidor!\n");
+        exit(1);
+    }
+    printf("Fim do envio\n");
 }
+
 
 int main(int argc, char *argv[]) {
-    char *NomeHost;
-    struct hostent *RegistroDNS;
 
-    /* Verifica parametros */
     if (argc != 3) {
-        printf("Uso correto: cliente <nome_serv> <porta>\n");
-        return 1;
+        printf("Você esqueceu de passar alguns parâmetros necessários, por favor rode do seguinte modo: \n");
+        printf("./cliente : <nome_do_servidor> <porta> \n");
+        exit(1);
     }
 
-    /* Abre o arquivo de logfile */
-    logfile = fopen("clienteudp.log","w");
-
-    /* Tenta obter o endereco IP do servidor */
-    NomeHost = argv[1];
-    if ((RegistroDNS = gethostbyname(NomeHost)) == NULL) {
-        printf("Nao obti o IP do servidor!\n");
-        return 2;
+    h = gethostbyname(argv[1]);
+    if (h == NULL) {
+        printf("Nao foi possível obter o IP do nome do servidor fornecido por parametro!\n");
+        exit(1);
     }
 
-    bcopy( (char *)RegistroDNS->h_addr, (char *)&EnderecRemoto.sin_addr, RegistroDNS->h_length );
-    EnderecRemoto.sin_family = RegistroDNS->h_addrtype;
-    EnderecRemoto.sin_port = htons(atoi(argv[2]));
+    remoteServAddr.sin_family = AF_INET;
 
-    printf("EnderecRemoto.sin_port = %d\n", EnderecRemoto.sin_port);
-    printf("EnderecRemoto.sin_family = %d\n", EnderecRemoto.sin_family);
-    for (int i = 0; i <  RegistroDNS->h_length; i++){
-        printf("EnderecRemoto.sin_addr[%d]= %d\n", i,  RegistroDNS->h_addr[i]);
+    bcopy( (char *)h->h_addr, (char *)&remoteServAddr.sin_addr, h->h_length );
+    for (int i = 0; i < h->h_length; i++){
+        printf("h->h_addr_list[i] = %d\n",   h->h_addr_list[i]);
     }
-    
-    fprintf(logfile,"Obtido IP do servidor.\n");
+    remoteServAddr.sin_port = htons(atoi(argv[2]));
 
-    /* Tenta criar um novo socket */
-    if ((sd = socket(RegistroDNS->h_addrtype,SOCK_DGRAM,0)) < 0) {
-        printf("Nao consegui abrir o socket!\n");
-        return 3;
+    printf("Configurações do enredeco -----------------------------\n");
+    printf("EnderecRemoto.sin_port = %d\n", remoteServAddr.sin_port);
+    printf("EnderecRemoto.sin_family = %d\n", remoteServAddr.sin_family);
+    printf("EnderecRemoto.sin_addr= %d\n",  remoteServAddr.sin_addr.s_addr);
+    printf("-----------------------------------------------------\n");
+
+    sd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sd < 0) {
+        printf("Nao foi possível abrir o socket!\n");
+        exit(1);
     }
-    fprintf(logfile,"Aberto um novo socket.\n");
 
-    /* Cria as threads que mandam e que recebem as mensagens */
-    fprintf(logfile,"Iniciando envio de pacotes...\n");
-    pthread_create(&R,NULL,(void *)receberMensagem,NULL);
-    fprintf(logfile,"Iniciando recebimento de pacotes...\n");
-    pthread_create(&S,NULL,(void *)enviarMensagem,NULL);
-
-    /* Aguarda o termino das threads */
-    pthread_join(S,NULL);
-    fprintf(logfile,"Terminado envio de pacotes.\n");
-    pthread_join(R,NULL);
-    fprintf(logfile,"Terminado recebimento de pacotes.\n");
-
-    printf("Pacotes enviados: %d.\n",PACKETS);
-    printf("Pacotes recebidos: %d (%.2lf %).\n",lidos,100.0*(double)lidos/(double)PACKETS);
-    printf("Latencia media: %lf milisegundos por mensagem.\n",(double)((double)tempo/(double)lidos)/1000.0);
+    enviarMensagem();
 
     close(sd);
-
-    fclose(logfile);
-
     return 0;
 }
--- Trabalho/servidor.c
+++ Trabalho/servidor.c
@@ -16,66 +16,57 @@ int main(int argc, char *argv[]) {
     int se;
     char buffer[SIZE];
     struct sockaddr_in EnderecLocal, EnderecCliente;
-    struct hostent *RegistroDNS;
+    struct hostent *dadosServidor;
     char NomeHost[MAXNOMEHOST];
 
-    signal();
-
-    /* Verifica argumentos */
     if (argc != 2) {
-        printf("Uso correto: ./server <porta>\n");
-        return 1;
+        printf("Você esqueceu de passar alguns parâmetros necessários, por favor rode do seguinte modo: \n");
+        printf("./servidor <porta>\n");
+        exit(1);
     }
 
-    /* Abre arquivo de log */
-    logfile = fopen("serverudp.log","w");
-    /* Tenta obter meu proprio endereco IP */
     gethostname(NomeHost, MAXNOMEHOST);
-    if ((RegistroDNS = gethostbyname(NomeHost)) == NULL) {
-        printf("Nao sei o meu proprio IP!\n");
-        return 2;
+    printf("NomeHost = %s\n", NomeHost);
+    dadosServidor = gethostbyname(NomeHost);
+    if (dadosServidor == NULL) {
+        printf("Nao foi possível obter IP do servidor!\n");
+        exit(1);
+    }
+
+    EnderecLocal.sin_family = AF_INET;
+    
+    bcopy( (char *)dadosServidor->h_addr, (char *)&EnderecLocal.sin_addr, dadosServidor->h_length );
+    for (int i = 0; i < dadosServidor->h_length; i++){
+        printf("h->h_addr_list[i] = %d\n",   dadosServidor->h_addr_list[i]);
     }
     EnderecLocal.sin_port = htons(atoi(argv[1]));
-    EnderecLocal.sin_family = RegistroDNS->h_addrtype;
-    bcopy( (char*)RegistroDNS->h_addr, (char *)&EnderecLocal.sin_addr, RegistroDNS->h_length );
 
-    fprintf(logfile,"Obtido meu proprio endereco IP.\n");
+    printf("Configurações do enredeco -----------------------------\n");
     printf("EnderecLocal.sin_port = %d\n", EnderecLocal.sin_port);
     printf("EnderecLocal.sin_family = %d\n", EnderecLocal.sin_family);
-    
-    for (int i = 0; i <  RegistroDNS->h_length; i++){
-        printf("nderecLocal.sin_addr[%d]= %d\n", i,  RegistroDNS->h_addr[i]);
+    printf("EnderecRemoto.sin_addr= %d\n",  EnderecLocal.sin_addr);
+    printf("-----------------------------------------------------\n");
+
+    se = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (se < 0) {
+        printf("Nao foi possível abrir o socket!\n");
+        exit(1);
     }
 
-
-    /* Tenta abrir um socket! */
-    if ((se = socket(RegistroDNS->h_addrtype,SOCK_DGRAM,0)) < 0) {
-        printf("Nao abri o socket!\n");
-        return 3;
-    }
-    fprintf(logfile,"Aberto o socket.\n");
-
-    /* Tenta fazer o bind para solicitar o uso da porta para o sistema
-     * operacional */
     if (bind(se, (struct sockaddr *)&EnderecLocal, sizeof(EnderecLocal)) < 0) {
-        printf("Nao fiz o bind. Porta em uso!\n");
+        printf("Nao foi possível executar o bind. Porta em uso!\n");
         return 4;
     }
-    fprintf(logfile,"Aberto a porta pelo bind.\n");
 
-    /* Le e repassa todos os pacotes */
     while (1) {
         unsigned int tt;
         int x;
         recvfrom(se,buffer,SIZE,0, (struct sockaddr *) &EnderecCliente, &tt);
         sscanf(buffer,"%d",&x);
-        fprintf(logfile,"Recebido pacote # %d.\n",x);
         printf("Recebido pacote # %d.\n",x);
-        sendto(se, buffer, SIZE, 0, (struct sockaddr *) &EnderecCliente, tt);
-        fprintf(logfile,"Enviado pacote # %d.\n",x);
-        fflush(logfile);
+        // sendto(se, buffer, SIZE, 0, (struct sockaddr *) &EnderecCliente, tt);
     }
-    fclose(logfile);
+
     close(se);
     return 0;
 }
