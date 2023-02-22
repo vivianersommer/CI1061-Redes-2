e28a4ed6e0b5c8fbf68c63d4a10d5d76c3056484
vods <vods18@inf.ufpr.br>
2023-02-21 21:27:46 -0300

    python atualizado, ainda há erro de envio, talvez no servidor

--- Trabalho/cliente.py
+++ Trabalho/cliente.py
@@ -1,28 +1,34 @@
 import socket
-import sys
+import sys 
 
-PACKETS = 50000
+PACKETS = 100000
 SIZE = 1024
 
 if len(sys.argv) != 3:
     print("Você esqueceu de passar alguns parâmetros necessários, por favor rode do seguinte modo: ")
-    print("./cliente : <nome_do_servidor> <porta>")
-    sys.exit(1)
+    print("./cliente : <nome_do_servidor> <porta> ")
+    exit(1)
 
-log_cliente = open("logCliente.txt", "w")
+log_cliente = open("logClientePython.txt", "w")
 
-endereco_servidor = (socket.gethostbyname(sys.argv[1]), int(sys.argv[2]))
+dados_servidor = socket.gethostbyname(sys.argv[1])
+if dados_servidor == None:
+    print("Nao foi possível obter o IP do nome do servidor fornecido por parametro!")
+    exit(1)
+log_cliente.write("Obtido IP do servidor.\n")
+
+endereco_servidor = (dados_servidor, int(sys.argv[2]))
 
 skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 log_cliente.write("Aberto um novo socket.\n")
 
 log_cliente.write("Iniciando envio de pacotes...\n")
 for i in range(PACKETS):
-    dados = f"{i:>{SIZE-1}}"
-    skt.sendto(dados.encode(), endereco_servidor)
-    log_cliente.write(f"Enviado pacote # {i}.\n")
-
+    dados = str(i).zfill(SIZE-1)
+    if skt.sendto(dados.encode(), endereco_servidor) != SIZE:
+        print("Nao foi possivel transmitir para o servidor!")
+        exit(1)
+    log_cliente.write(f"Enviado pacote #{i}.\n")
 log_cliente.write("Terminado envio de pacotes.\n")
 log_cliente.close()
 
--- Trabalho/servidor.py
+++ Trabalho/servidor.py
@@ -1,70 +1,78 @@
 import socket
+import struct
 import sys
 
+MAXNOMEHOST = 30
 SIZE = 1024
-MAXNOMEHOST = 128
-SEQUENCE_MAX = 100000
+SIZE_MAX = 500001
 
-def selection_sort(arr, n):
-    for i in range(n):
-        min_idx = i
-        for j in range(i+1, n):
-            if arr[j] < arr[min_idx]:
-                min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+def selection_sort(num, tam):
+    for i in range(tam - 1):
+        min = i
+        for j in range(i + 1, tam):
+            if num[j] > num[min]:
+                min = j
+        if i != min:
+            num[i], num[min] = num[min], num[i]
+
 
 def main():
-    skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
     if len(sys.argv) != 2:
-        print("Você esqueceu de passar alguns parâmetros necessários, por favor rode do seguinte modo: ")
-        print("./servidor <porta>")
+        print("Você esqueceu de passar alguns parâmetros necessários, por favor rode do seguinte modo:")
+        print("python3 servidor.py <porta>")
         sys.exit(1)
 
-    enderecoServidor = ('', int(sys.argv[1]))
-    skt.bind(enderecoServidor)
+    log_servidor = open("logServidorPython.txt", "w")
 
-    sequencia = [0] * SEQUENCE_MAX
-    sequenciaPos = 0
-    mensagensRecebidas = 0
+    nome_host = socket.gethostname()
+    dados_servidor = socket.gethostbyname(nome_host)
+    if not dados_servidor:
+        print("Nao foi possível obter IP do servidor!\n")
+        sys.exit(1)
+    log_servidor.write("Obtido meu proprio endereco IP.\n")
 
+    endereco_servidor = (dados_servidor, int(sys.argv[1]))
+    skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    skt.bind(endereco_servidor)
+    log_servidor.write("Aberto o socket.\n")
+    log_servidor.write("Aberto a porta para comunicação.\n")
+
+    tv = struct.pack('ll', 5, 0)
+    skt.setsockopt(socket.SOL_SOCKET, socket.SO_RCVTIMEO, tv)
+
+    sequencia = []
     while True:
-        dados, enderecoCliente = skt.recvfrom(SIZE)
+        dados, endereco_cliente = skt.recvfrom(SIZE)
         if not dados:
-            print("Servidor finalizando após 5 segundos sem novas mensagens.")
+            log_servidor.write("Servidor finalizando após 5 segundos sem novas mensagens.\n")
             break
         x = int(dados.decode('utf-8'))
-        sequencia[sequenciaPos] = x
-        sequenciaPos += 1
-        mensagensRecebidas += 1
-        print(f"Recebido pacote #{x}.")
+        sequencia.append(x)
+        log_servidor.write(f"Recebido pacote # {x}.\n")
+    log_servidor.write("Fim dos recebimentos...\n")
+    log_servidor.write("----------------------------------------------------------------------------------\n")
+    log_servidor.write("Verificando ordem e perdas de mensagens...\n")
 
-    print("Fim dos recebimentos...")
-    print("----------------------------------------------------------------------------------")
-    print("Verificando ordem e perdas de mensagens...")
+    mensagens_recebidas = len(sequencia)
+    fora_de_ordem = 0
+    for q in range(mensagens_recebidas - 1):
+        if sequencia[q] > sequencia[q + 1]:
+            fora_de_ordem += 1
 
-    foraDeOrdem = 0
-    print(f"mensagensRecebidas = {mensagensRecebidas}")
-    for q in range(mensagensRecebidas - 1):
-        if sequencia[q] > sequencia[q+1]:
-            foraDeOrdem += 1
-            print(f"Achei um fora de ordem, os meliantes: {sequencia[q]} e {sequencia[q+1]}")
-
-    selection_sort(sequencia, mensagensRecebidas)
-
-    q = 0
+    selection_sort(sequencia, mensagens_recebidas)
     perdidos = 0
-    while q < mensagensRecebidas:
-        if q == sequencia[q]:
-            q += 1
-        else:
-            q = sequencia[q]
+    final = sequencia[mensagens_recebidas - 1]
+    for i in range(final):
+        if i not in sequencia:
             perdidos += 1
 
-    print(f"Foram recebidas {mensagensRecebidas} mensagens, perdeu {perdidos} e {foraDeOrdem} fora de ordem.")
-    print("Finalizada a verificação...")
+    log_servidor.write(f"Foram recebidas {mensagens_recebidas} mensagens, perdeu {perdidos} e {fora_de_ordem} fora de ordem.\n")
+    log_servidor.write("Finalizada a verificação...\n")
 
+    log_servidor.close()
     skt.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
